9dd32bad650db77bab6c3cc6336bd5a54a404c4c
Conor <G00373582@gmit.ie>
2022-02-09 10:14:19 +0000

    Additional tests added for title check

--- src/test/java/ie/gmit/passenger/PassengerTest.java
+++ src/test/java/ie/gmit/passenger/PassengerTest.java
@@ -1,15 +1,22 @@
 package ie.gmit.passenger;
-
-import ie.gmit.passenger.Passenger;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PassengerTest {
-    Passenger pass = new Passenger("Mr");
+    private Passenger pass;
 
     @Test
     void TestValidTitle() {
+        pass = new Passenger("Mr");
         assertEquals("Mr", pass.getTitle());
+    }
+
+    @Test
+    void TestInvalidTitle() {
+        final String invalid = "Invalid title provided";
+        Exception exceptionThrown = assertThrows(IllegalArgumentException.class, ()-> new Passenger("Miss"));
+        assertEquals(invalid, exceptionThrown.getMessage());
     }
 
 }
