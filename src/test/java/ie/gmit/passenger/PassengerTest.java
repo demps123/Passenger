84ace240ce006949aa202b8c6a21b217a738102b
Conor <G00373582@gmit.ie>
2022-02-09 10:24:15 +0000

    Additional tests added for title check

--- src/test/java/ie/gmit/passenger/PassengerTest.java
+++ src/test/java/ie/gmit/passenger/PassengerTest.java
@@ -4,18 +4,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PassengerTest {
+
     private Passenger pass;
 
     @Test
     void TestValidTitle() {
-        pass = new Passenger("Mr");
+        pass = new Passenger("Mr", "Conor");
         assertEquals("Mr", pass.getTitle());
     }
-
     @Test
     void TestInvalidTitle() {
         final String invalid = "Invalid title provided";
-        Exception exceptionThrown = assertThrows(IllegalArgumentException.class, ()-> new Passenger("Miss"));
+        Exception exceptionThrown = assertThrows(IllegalArgumentException.class, ()-> new Passenger("Miss","Conor"));
+        assertEquals(invalid, exceptionThrown.getMessage());
+    }
+
+    @Test
+    void TestValidName() {
+        pass = new Passenger("Mr", "Con");
+        assertEquals("Con", pass.getName());
+    }
+    @Test
+    void TestInvalidName() {
+        final String invalid = "Invalid name provided";
+        Exception exceptionThrown = assertThrows(IllegalArgumentException.class, ()-> new Passenger("Ms","Co"));
         assertEquals(invalid, exceptionThrown.getMessage());
     }
 
